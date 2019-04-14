0b3652cefb7bd71151c3ecbd39f9ac48ed092ac4
Manny Ikomi <41631324+MannyIkomi@users.noreply.github.com>
2019-04-14 14:58:51 -0400

    add modules union list to the project schema

--- schema.js
+++ schema.js
@@ -4,7 +4,8 @@ const {
   GraphQLID,
   GraphQLSchema,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLUnionType
 } = require('graphql')
 const axios = require('axios')
 
@@ -17,10 +18,53 @@ const BE_API_KEY = process.env.BE_API_KEY
 const ProjectCoversType = new GraphQLObjectType({
   name: 'Cover',
   fields: () => ({
-    original: { type: GraphQLString },
-    '404': { type: GraphQLString },
-    '808': { type: GraphQLString }
+    original: { type: GraphQLString }
+    // '404': { type: GraphQLString },
+    // '808': { type: GraphQLString }
   })
+})
+
+const ImageSizesType = new GraphQLObjectType({
+  name: 'ImageSize',
+  fields: () => ({
+    original: { type: GraphQLString },
+    // '1400': { type: GraphQLString },
+    disp: { type: GraphQLString }
+  })
+})
+
+const ImageModule = new GraphQLObjectType({
+  name: 'ImageModule',
+  fields: () => ({
+    project_id: { type: GraphQLInt },
+    type: { type: GraphQLString },
+    sizes: { type: ImageSizesType }
+  })
+})
+
+const TextModule = new GraphQLObjectType({
+  name: `TextModule`,
+  fields: () => ({
+    project_id: { type: GraphQLInt },
+    type: { type: GraphQLString },
+    text_plain: { type: GraphQLString },
+    text: { type: GraphQLString }
+  })
+})
+
+const ProjectModulesType = new GraphQLUnionType({
+  name: 'ProjectModules',
+  types: [ImageModule, TextModule],
+  resolveType: value => {
+    if (value.type === 'image') {
+      return ImageModule
+    }
+    if (value.type === 'text') {
+      return TextModule
+    } else {
+      return null
+    }
+  }
 })
 
 const ProjectType = new GraphQLObjectType({
@@ -33,8 +77,10 @@ const ProjectType = new GraphQLObjectType({
     published_on: { type: GraphQLInt }, //epoch date
     url: { type: GraphQLString },
     slug: { type: GraphQLString },
-    fields: { type: GraphQLList(GraphQLString) } //as in creative field categories
+    fields: { type: GraphQLList(GraphQLString) }, //as in creative field categories
     // covers
+    // modules
+    modules: { type: GraphQLList(ProjectModulesType) }
   })
 })
 
